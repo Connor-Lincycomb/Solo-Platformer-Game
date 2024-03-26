166d06d005c099659f490174da87c10fd32ebba8
Connor Lincycomb <connoroverwatchalt@gmail.com>
2024-03-25 22:34:01 -0500

    Enemy Hitboxes

--- spikes.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-
-# this "is Player" references a class_name Player we instantiated in the player.gd script, 2nd line.
-# this function restarts the scene when Player contacts the spikes
-func _on_body_entered(body):
-	if body is Player:
-		get_tree().reload_current_scene()
